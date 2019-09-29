56e5e8504b549d3af2d8c130eb4a9eebb55213f3
Makito Katsube <katsubemakito@gmail.com>
2019-09-29 17:22:13 +0900

    add sql/init.sql

---
+++ sql/init.sql
@@ -0,0 +1,26 @@
+/**
+ * DBとテーブルの準備
+ */
+
+/*----------------------------*/
+/* DB作成                     */
+/*----------------------------*/
+CREATE DATABASE neecLogin;
+USE neecLogin;
+
+/*----------------------------*/
+/* テーブルを作成             */
+/*----------------------------*/
+CREATE TABLE User(
+  `id` varchar(32),     /* ID */
+  `pw` varchar(255),    /* パスワード */
+)
+ENGINE=InnoDB   /* MySQLのエンジンを指定 */
+CHARSET=utf8;   /* 文字コード */
+
+/*----------------------------*/
+/* 初期データを挿入           */
+/*----------------------------*/
+INSERT INTO User (id,pw) VALUES
+	('foo@example.com', 'apple'),
+	('bar@example.net', 'orange');
