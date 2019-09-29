4ae2ef81143d64890c90f6ac27e937c14b0f6f9a
Makito Katsube <katsubemakito@gmail.com>
2019-09-29 19:42:11 +0900

    add sample4

---
+++ auth4.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * ユーザー認証API - DB版 (SQLインジェクション)
+ *
+ * @version 1.0.0
+ * @author M.Katsube <katsubemakito@gmail.com>
+ */
+
+//-------------------------------
+// DBの接続情報
+//-------------------------------
+define('DB_DSN',  'mysql:dbname=neecLogin;host=localhost');	// 接続先
+define('DB_USER', 'root');			// ユーザーID
+define('DB_PW',   'H@chiouji1');	// パスワード
+
+//-------------------------------
+// 引数を受け取る
+//-------------------------------
+$email    = isset($_REQUEST['email'])?  $_REQUEST['email']:null;
+$password = isset($_REQUEST['password'])? $_REQUEST['password']:null;
+
+// 入力値をチェック
+if( ($email === null) || ($password === null) ){
+	putResult(false, 'email and password is required');
+	return(false);
+}
+
+//-------------------------------
+// 認証
+//-------------------------------
+// 成功
+if( authDB($email, $password) ){
+	setcookie('uname', $email, (time()+(60*60*24*365)));	//1年間有効なCookieを発行
+	putResult(true);
+}
+// 失敗
+else{
+	putResult(false, 'email or password is wrong');
+}
+
+
+/**
+ * ユーザー認証
+ *
+ * @param string $id
+ * @param string $pw
+ * @return boolean
+ */
+function authDB(string $id, string $pw){
+	// SQLの準備
+	$sql = sprintf('SELECT count(*) as cnt FROM User WHERE id=\'%s\' and pw=\'%s\'', $id, $pw);		//★セキュリティホール
+
+	// SQLを実行
+	$dbh = new PDO(DB_DSN, DB_USER, DB_PW);  // 接続
+	$sth = $dbh->prepare($sql);     		 // SQL準備
+	$sth->execute();                		 // 実行
+
+	// 実行結果を取得
+	$result = $sth->fetch(PDO::FETCH_ASSOC);
+	return( ($result !== false) && ($result['cnt'] > 0) );
+}
+
+/**
+ * 結果をJSON形式で表示
+ *
+ * @param boolean $result
+ * @param mixed [$value=null]
+ * @return void
+ */
+function putResult(bool $result, $value=null){
+	header('Content-type: application/json');
+	echo json_encode([
+		'head' => [
+			'result' => $result,
+			'time'   => time()
+		],
+		'body' => $value
+	]);
+}
